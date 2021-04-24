f1124e4031c2dd298c29380595d8f4cd607a1765
Merve Parlak <parlakmervepmm@gmail.com>
2021-04-24 22:07:33 +0300

    Add files via upload

---
+++ ComputeExpression.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaexcercises;
+
+/**
+ *
+ * @author Asus
+ */
+public class ComputeExpression {
+
+    public static void main(String args[]){
+//      System.out.print("(15.3-5*2)/(45*2.1=)");
+//      System.out.print((15.3-5*2)/(45*2.1));
+        /* Concatenation sign "+" */
+        System.out.println("(15.3-5*2)/(45*2.1=)"+((15.3-5*2)/(45*2.1)));
+    }
+}
